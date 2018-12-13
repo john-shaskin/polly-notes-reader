b27e9be9815131fe67f6c96c11745d84d00e7d48
John Shaskin <guitarjohn@gmail.com>
2018-12-12 23:10:52 -0700

    Add s3 buckets for static website and mp3 output

--- lib/polly-notes-reader-stack.ts
+++ lib/polly-notes-reader-stack.ts
@@ -8,16 +8,31 @@ export class PollyNotesReaderStack extends cdk.Stack {
 
     // TODO: This stack will contain
     // 1. s3 bucket for public website (public access??)
-    const websiteBucket = new s3.Bucket(this, 'polly-reader-website');
+    const websiteBucket = new s3.Bucket(this, 'PollyReaderStaticWebsite', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html'
+    });
+    websiteBucket.grantPublicAccess('*', 's3:GetObject');
     // 2. s3 bucket for dropping mp3s (public access??)
+    new s3.Bucket(this, 'PollyReaderMP3Bucket');
     // 3. API gateway with lambda endpoints
     //    a. POST new notes
     //    b. GET notes
     // 4. DynamoDB table to store notes
-    new dynamoDb.Table(this, 'posts', {
+    new dynamoDb.Table(this, 'PostsTable', {
+      tableName: 'polly-posts',
       partitionKey: { name: 'id', type: dynamoDb.AttributeType.String }
     });
     // 5. SNS topic to trigger when new notes added
     // 6. Lambda listening to SNS topic that converts the text to mp3 audio
   }
+  
+  toCloudFormation() {
+    const res = super.toCloudFormation();
+    
+    // TODO: Manually fuck with the CFN
+
+    return res;
+  }
+  
 }
